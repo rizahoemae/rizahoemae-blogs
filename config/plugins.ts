5b66ff7b1a7fd70a75c46658a61edbc7a7f5ae81
rizahoemae <humaida.mawaddatur@gmail.com>
2026-03-23 02:30:25 +0700

    feat: add blogs, experiences, skills, projects

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,19 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  "strapi-csv-import-export": {
+    enabled: true,
+    config: {
+      authorizedExports: ["api::skill.skill"],
+      authorizedImports: [
+        "api::skill.skill",
+        "api::experience.experience",
+        "api::project.project",
+      ],
+    },
+  },
+});
 
 export default config;
